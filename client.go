364674a4a257ef341d61627deb6efc7e50ee90c2
AppleTruffle <appletrufflemuffinkinz@gmail.com>
2025-03-07 20:23:26 -0700

    pokeapi client and location_list updated

--- client.go
+++ client.go
@@ -3,16 +3,20 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/lasantos2/pokedex/internal/pokecache"
 )
 
 // Client -
 type Client struct {
+	cache pokecache.Cache
 	httpClient http.Client
 }
 
-// NewClient -
-func NewClient(timeout time.Duration) Client {
+// NewClient
+func NewClient(timeout time.Duration, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
